4e60321afbb8aa79ab0c3010aa4f87e8594fc851
Hyeonseok Yoo <evermongwan@gmail.com>
2018-12-06 21:40:50 +0900

    print json

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+import json
+from parser import parse
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main_page():
+    t = 1
+    bus_stop_list = []
+
+    for i in parse():
+        bus_stop_list.append(["busStop{}".format(t), i])
+        t += 1
+
+    return json.dumps(dict(bus_stop_list))
 
 
 if __name__ == '__main__':
--- parser.py
+++ parser.py
@@ -1,16 +1,19 @@
 from urllib import request
 import xml.etree.ElementTree as ET
 
+parse_function_mapping = {
+    ""
+}
+
 
 def parse():
-    secretKeyFile = open('secretKey', 'r') # github 업로드를 위해 시크릿키 파일을 분리했습니다.
-    key = secretKeyFile.readline()
-    routeId = 100100185  # 1711 고유 아이디
+    key = open('secretKey', 'r').readline()  # github 업로드를 위해 시크릿키 파일을 분리했습니다.
+    route_id = 100100185  # 1711 고유 아이디
 
     # 국민대학교 정류장 고유번호 : 08110
 
     url = 'http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRouteAll?'
-    param = "ServiceKey=" + key + "&busRouteId=" + str(routeId)
+    param = "ServiceKey=" + key + "&busRouteId=" + str(route_id)
 
     with request.urlopen(url + param) as req:
         response_body = req.read().decode('utf-8')
@@ -21,8 +24,9 @@ def parse():
         # f.close() 추후 DB 입출력을 위해 임시 주석처리
 
     for i in root[2]:  # root[2]는 <msgBody>를 의미
-        print(i.findtext('stNm'))  # i는 itemList, 각 itemList 에서 stNm 값 가져오기
+        yield i.findtext('stNm')  # i는 itemList, 각 itemList 에서 stNm 값 가져오기
 
 
 if __name__ == "__main__":
-    parse()
+    for i in parse():
+        print(i)
